ab2260c5d74f09367a03d76a030ab3c33a29c578
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-25 11:32:08 +0545

    Create AccountController and LoginView

---
+++ BlogApp.Web/Models/LoginVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Web.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
