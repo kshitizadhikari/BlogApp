115633edfba67d7120fc9e9634f7715ac41477e0
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-25 15:25:09 +0545

    add logout in view

--- BlogApp.Web/Controllers/AccountController.cs
+++ BlogApp.Web/Controllers/AccountController.cs
@@ -58,7 +58,7 @@ namespace BlogApp.Web.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login");
         }
     }
 }
