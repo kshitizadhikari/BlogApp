9952d8157e0cd2edf40a89ece6289b7f4f65e8e7
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-25 17:48:09 +0545

    create RegisterVM

---
+++ BlogApp.Web/Models/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Web.Models
+{
+    public class RegisterVM
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm Password field must match Password field")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword{ get; set; }
+    }
+}
