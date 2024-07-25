3bc85b3219bdf8e3fcbde8dfd5e3d33d1e9f0ad3
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-25 12:29:06 +0545

    add login logic

--- BlogApp.Web/Controllers/AccountController.cs
+++ BlogApp.Web/Controllers/AccountController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApp.Web.Infrastructure.Interfaces;
+using BlogApp.Web.Models;
+using BlogApp.Web.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogApp.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IRepositoryWrapper _repository;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(IRepositoryWrapper repository, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Email", "Invalid Email or Password");
+                return View(loginVM);
+            }
+
+            AppUser user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "Invalid Email or Password");
+                return View(loginVM);
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginVM.Password, false);
+
+            if(result == null)
+            {
+                ModelState.AddModelError("Email", "Invalid Email or Password");
+                return View(loginVM);
+            }
+
+            await _signInManager.SignInAsync(user, true);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
