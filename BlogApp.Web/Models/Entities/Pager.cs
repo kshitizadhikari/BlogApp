d6388ac3664b291ad468d1ed46c2b7b9ff3a1337
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-30 11:42:32 +0545

    create a Pager model

---
+++ BlogApp.Web/Models/Entities/Pager.cs
@@ -0,0 +1,51 @@
+﻿namespace BlogApp.Web.Models.Entities
+{
+    public class Pager
+    {
+        public int TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int PageSize { get; private set; }
+        public int TotalPages { get; private set; }
+        public int StartPage{ get; private set; }
+        public int EndPage { get; private set; }
+
+        public Pager()
+        {
+            
+        }
+
+        public Pager(int totalItems, int curPage, int pageSize = 10)
+        {
+            int totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)pageSize);
+            int currentPage = curPage;
+
+            int startPage = currentPage - 5;
+            int endPage = currentPage + 4;
+
+            if (startPage <= 0)
+            {
+                endPage = endPage - (StartPage - 1);
+                startPage = 1;
+            }
+
+            if (endPage > 10)
+            {
+                if(endPage > totalPages)
+                {
+                    endPage = totalPages;
+                }
+                startPage = endPage - 9;
+            }
+
+            TotalItems = totalItems;
+            CurrentPage = curPage;
+            PageSize = pageSize;
+            TotalPages = totalPages;
+            StartPage = startPage;
+            EndPage = endPage;
+
+        }
+    }
+
+   
+}
