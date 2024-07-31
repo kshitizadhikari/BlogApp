2b719f78e721e3e78873baade1bd6bc9c9449eb0
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-31 13:02:28 +0545

    add ChangePasswordVM

---
+++ BlogApp.Web/Models/ChangePasswordVM.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Web.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.Web.Models
+{
+    public class ChangePasswordVM
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Current Password")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "This field must match the NewPassword field")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+
+    }
+}
--- BlogApp.Web/Models/EditCommentVM - Copy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlogApp.Web.Models
-{
-    public class EditCommentVM
-    {
-        public int Id { get; set; }
-        public string Content { get; set; }
-
-    }
-}
