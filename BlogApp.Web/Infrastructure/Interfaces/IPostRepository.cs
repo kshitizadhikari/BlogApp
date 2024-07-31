f07164cd9d85cc68a928173fea4677be0bfed2a9
kshitiz <adhikarikshitiz12@gmail.com>
2024-07-31 10:24:29 +0545

    add GetPostByID action in PostRepository

--- BlogApp.Web/Infrastructure/Interfaces/IPostRepository.cs
+++ BlogApp.Web/Infrastructure/Interfaces/IPostRepository.cs
@@ -6,5 +6,7 @@ namespace BlogApp.Web.Infrastructure.Interfaces
     public interface IPostRepository: IBaseRepository<Post>
     {
         Task<List<Comment>> GetComments(int postId);
+
+        Task<Post> GetPostById(int id);
     }
 }
--- BlogApp.Web/Infrastructure/Repositories/PostRepository.cs
+++ BlogApp.Web/Infrastructure/Repositories/PostRepository.cs
@@ -14,5 +14,22 @@ namespace BlogApp.Web.Infrastructure.Repositories
         {
             return _dbContext.Comments.Where(c => c.PostId == postId).ToList();
         }
+
+        public async Task<Post> GetPostById(int id)
+        {
+            Post? post = await _dbContext.Posts.FindAsync(id);
+            AppUser? author = await _dbContext.Users.FindAsync(post.AppUserId);
+            List<Comment> comments = await GetComments(id);
+
+            foreach (var comment in comments)
+            {
+                AppUser? commentor = await _dbContext.Users.FindAsync(comment.AppUserId);
+                comment.AppUser = commentor;
+            }
+
+            post.AppUser = author;
+            post.Comments = comments;
+            return post;
+        }
     }
 }
